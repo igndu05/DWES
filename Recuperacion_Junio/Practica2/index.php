279ec0bcd760850957cf1aed3d9a0da635982784
Ignacio <idurmes337@g.educaand.es>
2026-04-09 10:12:34 +0200

    9-4

--- Recuperacion_Junio/Practica2/index.php
+++ Recuperacion_Junio/Practica2/index.php
@@ -1,11 +1,51 @@
+<?php
+    if(isset($_POST["btnEntrar"]) || isset($_POST["btnRegistrarse"]))
+    {
+        $error_usuario = $_POST["usuario"] == "";
+        $error_clave = $_POST["clave"] == "";
+
+        $error_login = $error_usuario || $error_clave;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Practica 2</title>
+    <title>Practica Rec 2</title>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-    OMFG
+    <h1>Práctica Rec 2</h1>
+    <form action="index.php" method="post">
+        <p>
+            <label for="usuario">Usuario:</label>
+            <input type="text" name="usuario" id="usuario"/>
+            <?php
+            if((isset($_POST["btnEntrar"]) && $error_usuario) || (isset($_POST["btnRegistrarse"]) && $error_usuario))
+            {
+                echo "<span class='error'>* Debe añadir un usuario *</span>";
+            }
+            ?>
+        </p>
+        <p>
+            <label for="clave">Contraseña:</label>
+            <input type="password" name="clave" id="clave"/>
+            <?php
+            if((isset($_POST["btnEntrar"]) && $error_clave) || (isset($_POST["btnRegistrarse"]) && $error_clave))
+            {
+                echo "<span class='error'>* Debe añadir una contraseña *</span>";
+            }
+            ?>
+        </p>
+        <p>
+            <button type="submit" name="btnEntrar">Entrar</button>
+            <button type="submit" name="btnRegistrarse">Registrarse</button>
+        </p>
+    </form>
 </body>
 </html>
